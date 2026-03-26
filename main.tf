b67db9387ee077fc23c643c8b927f6c2141f8a21
Balu-j <balajijanjanamm@gmail.com>
2026-03-27 04:09:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
-count = 10
+count = 5
   ami           = "ami-02dfbd4ff395f2a1b" 
   instance_type = "t2.micro"
-
-  tags = {
-    Name        = "DevOps-Practice-Server"
-  }
 }
